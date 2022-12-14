5e9f3cc4ee283a7f033e9969d8a9169a0a86e6a4
WantedStream <48957819+WantedStream@users.noreply.github.com>
2022-12-14 21:19:29 +0200

    added rishoni

--- 8086/16-bit/DOS/10/rishon.asm
+++ 8086/16-bit/DOS/10/rishon.asm
@@ -1,7 +1,10 @@
 .MODEL SMALL
 .STACK 100h
 .DATA
-var dw 21
+var dw 17
+divby dw 2
+times dw 0
+tmp dw 0
 .CODE
 
 start:
@@ -9,20 +12,40 @@ start:
 mov ax,@Data
 mov ds,ax
 mov ax,0
-mov bl,2
+
+
 again:
-
 mov ax,var
-
+mov times,0
+mov tmp,0
 
 subb:
-sub ax,bl
-cmp bl,var
-jb sb
+add times,1
+sub ax,divby
+cmp ax,1
+jg subb
 
 
-loop again
-mov ax,4c00h
+mov ax,divby
+
+mult:
+add tmp,ax
+sub times,1
+cmp times,0
+jg mult
+
+mov ax,tmp
+cmp ax,var
+je ending
+
+
+add divby,1
+mov dx,divby
+cmp dx,var
+jl again
+
+mov bl,1
+ending:mov ax,4c00h
 int 21h
 
 
